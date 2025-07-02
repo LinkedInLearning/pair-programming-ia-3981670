747f3c66aec94af1e0b712f0e8344ba92057f7df
Carlos Solis <carlos.solis.d@gmail.com>
2025-07-02 04:31:56 -0400

    base

--- src/pages/Nosotros.jsx
+++ src/pages/Nosotros.jsx
@@ -1,7 +1,15 @@
 function Nosotros() {
+
+  // Listado JSON con 3 miembros de junta directiva
+  // estructura: [{nombre:"juan pérez", cargo:"Director"}]
+
   return (
     <>
     <h1>Sobre Nosotros</h1>
+
+    // Texto de bienvenida 
+
+    // bucle listando miembros de junta directiva
     </>
   )
 }
